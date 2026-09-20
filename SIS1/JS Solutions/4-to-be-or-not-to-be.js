f2b2ecea5bc1ac1a00957a88a3590152cd123f53
zhanfx <t_zhankylysh@kbtu.kz>
2026-09-20 21:00:24 +0500

    Implement expect function for value assertions
    Added an 'expect' function for value comparison with 'toBe' and 'notToBe' methods.

--- SIS1/JS Solutions/4-to-be-or-not-to-be.js
+++ SIS1/JS Solutions/4-to-be-or-not-to-be.js
@@ -1,1 +1,12 @@
-
+var expect = function (val) {
+  return {
+    toBe: (v) => {
+      if (val === v) return true;
+      throw new Error("Not Equal");
+    },
+    notToBe: (v) => {
+      if (val !== v) return true;
+      throw new Error("Equal");
+    },
+  };
+};
