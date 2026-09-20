27364b6df8e9db587d24735066f346f317ce3113
zhanfx <t_zhankylysh@kbtu.kz>
2026-09-20 21:00:52 +0500

    Add snail traversal method to Array prototype

--- SIS1/JS Solutions/5-snail-traversal.js
+++ SIS1/JS Solutions/5-snail-traversal.js
@@ -1,1 +1,11 @@
-
+Array.prototype.snail = function (rowsCount, colsCount) {
+  if (this.length !== rowsCount * colsCount) return [];
+  const res = Array.from({ length: rowsCount }, () => []);
+  for (let i = 0; i < this.length; i++) {
+    const col = Math.floor(i / rowsCount);
+    const idx = i % rowsCount;
+    const row = col % 2 === 0 ? idx : rowsCount - 1 - idx;
+    res[row][col] = this[i];
+  }
+  return res;
+};
