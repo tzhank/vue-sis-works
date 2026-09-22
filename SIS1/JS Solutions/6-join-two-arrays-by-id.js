ee4164daa988c13a1a6ec203c30b68846f07bd98
zhanfx <t_zhankylysh@kbtu.kz>
2026-09-22 17:56:50 +0500

    Update 6-join-two-arrays-by-id.js

--- SIS1/JS Solutions/6-join-two-arrays-by-id.js
+++ SIS1/JS Solutions/6-join-two-arrays-by-id.js
@@ -2,7 +2,7 @@ var join = function (arr1, arr2) {
   const map = new Map();
   for (const o of arr1) map.set(o.id, o);
   for (const o of arr2) {
-    map.set(o.id, map.has(o.id) ? { ...map.get(o.id), ...o } : o); // arr2 overrides arr1
+    map.set(o.id, map.has(o.id) ? { ...map.get(o.id), ...o } : o); 
   }
   return [...map.values()].sort((a, b) => a.id - b.id);
 };
